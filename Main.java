8f1571a63aa71b2d601ac844c3e85f050eac9fb9
cicadariver <cicadariver@gmail.com>
2026-04-02 17:09:24 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 28;
+		String name = "KK";
+
+		System.out.println("私は"+ name + "です。年齢は" + age + "歳です。" );
+
+
+
+	}
+}
